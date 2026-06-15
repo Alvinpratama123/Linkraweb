b30d8ee431603e34a98267a37453c1656e57c4ee
Alvinpratama123 <pratama.alvin.muhammad@gmail.com>
2026-06-15 15:52:00 +0700

    p

--- src/pages/dashboardAdmin/components/membersModul.js
+++ src/pages/dashboardAdmin/components/membersModul.js
@@ -77,7 +77,6 @@ export default function MembersModul({ members = null }) {
                 required
               />
             </label>
-            
 
             <label className="space-y-2">
               <span className="text-sm font-medium text-gray-700">Email</span>
